1192f635282fcadace445099cdba534d194460b7
ykkssyaa <mr.ender.03@mail.ru>
2024-05-13 14:29:23 +0300

    move interfaces in one place

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/ykkssyaa/Bash_Service/internal/gateway"
+	"github.com/ykkssyaa/Bash_Service/internal/models"
 	"github.com/ykkssyaa/Bash_Service/pkg/logger"
 )
 
@@ -13,4 +14,11 @@ func NewService(gateways *gateway.Gateways, logger *logger.Logger) *Services {
 	return &Services{
 		Command: NewCommandService(gateways.Command, gateways.Storage, gateways.CommandCache, logger),
 	}
+}
+
+type Command interface {
+	CreateCommand(script string) (models.Command, error)
+	GetCommand(id int) (models.Command, error)
+	GetAllCommands(limit, offset int) ([]models.Command, error)
+	StopCommand(id int) error
 }
